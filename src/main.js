04a24f0e970806dd99308e3c2451663788c5b29a
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-11-13 08:26:59 +0100

    011

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-import App from './App.svelte';
-
-const app = new App({
-	target: document.body,
-	props: {}
-});
-
+import TodoList from './TodoList.svelte';
+ 
+const app = new TodoList({target: document.body});
+ 
 export default app;
