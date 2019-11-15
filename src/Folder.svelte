d5199802e65dbeb31494b66f7aaf9b5737b31a1f
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-11-15 09:05:05 +0100

    x

---
+++ src/Folder.svelte
@@ -0,0 +1,44 @@
+<script>
+
+	// export let expanded = false
+	// export let name
+	export let tree
+	export let path=[]
+
+</script>
+
+<style>
+	span {
+		padding: 0 0 0 1.5em;
+		background-size: 1em 1em;
+		font-weight: bold;
+		cursor: pointer;
+	}
+
+	ul {
+		padding: 0.2em 0 0 0.5em;
+		margin: 0 0 0 0.5em;
+		list-style: none;
+		border-left: 1px solid #eee;
+	}
+
+	li {
+		padding: 0.2em 0;
+	}
+</style>
+
+<span class:expanded on:click={()=>expanded = !expanded}>{name}</span>
+
+{#if expanded}
+	<ul>
+		{#each files as file}
+			<li>
+				{#if file.type === 'folder'}
+					<svelte:self {...file}/>
+				{:else}
+					<span>{file.name}</span>
+				{/if}
+			</li>
+		{/each}
+	</ul>
+{/if}
