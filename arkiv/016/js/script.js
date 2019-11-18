7b3fa04bcfa0190873a831a12fc446d5b349f844
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-11-18 01:35:24 +0100

    x

--- arkiv/016/js/script.js
+++
@@ -1,5 +0,0 @@
-let myButton = document.querySelector('button');
-let showSite = document.querySelector('.hidden');
-myButton.addEventListener('click', () => {
-  showSite.classList.toggle('show');
-});
