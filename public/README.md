762823dc82acf1ce6a1ab4964444a78fa749cb00
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-11-22 14:01:54 +0100

    x

--- public/README.md
+++
@@ -1,3 +0,0 @@
-[Try it!](https://christernilsson.github.io/svelte-deploy/002)
-
-Thanks to https://www.youtube.com/watch?v=xaIK4nmQqxw
--- src/App.svelte
+++ src/App.svelte
@@ -2,19 +2,21 @@
 	import range from 'lodash.range'
 	import sample from 'lodash.sample'
 
-	const tw = 10
-	const th = 20
-	const cc = 30
+	const TW = 10
+	const TH = 20
+	const CC = 30
+
 	const cell = []
-	const matrix = []
+	const matrix = [] // used for drawing with svg
 	let x = 3
 	let y = -1
 	let tm = 0
 	let dwn = 0
-	for (const r of range(th)) {
+	
+	for (const r of range(TH)) {
 		cell[r] = []
 		matrix[r] = []
-		for (const c of range(tw)) {
+		for (const c of range(TW)) {
 			cell[r][c] = 0
 			matrix[r][c] = 0
 		}
@@ -23,33 +25,33 @@
 	const gen = () => '0000' + sample(shape) + '0000' 
 	let csh = gen()
 
-	function dr(type,row) { 
-		for (const r of range(th)) {
+	const dr = (type,row) => { 
+		for (const r of range(TH)) {
 			let cnt = 0
-			for (const c of range(tw)) {
+			for (const c of range(TW)) {
 				matrix[r][c] = 0
 				if (cell[r][c]) {
 					matrix[r][c] = 1 
 					cnt++
 				}
 				
-				if (type==2 && th-r<row+1) cell[th-r][c] = cell[th-r-1][c]
+				if (type==2 && TH-r<row+1) cell[TH-r][c] = cell[TH-r-1][c]
 			}
-			if (cnt==tw) {
-				for (const c of range(tw)) cell[r][c] = 0
+			if (cnt==TW) {
+				for (const c of range(TW)) cell[r][c] = 0
 				dr(2,r)
 			}
 		}
 	}
 
-	function chk(type,n=0) { 
+	const chk = (type,n=0) => { 
 		let out = ''
 		let fnd = 0
 		for (let r of range(4)) {
 			for (let c of range(4)) {
 				if (csh[c+r*4] == 1) {
 					if (type == 1) matrix[r+y][c+x] = 1
-					if (type == 2) if (r+y>th-2 || cell[r+y+1][c+x] == 1) {
+					if (type == 2) if (r+y>TH-2 || cell[r+y+1][c+x] == 1) {
 						chk(3)
 						csh = gen()
 						x=3
@@ -57,7 +59,7 @@
 						dwn=0
 					}
 					if (type == 3) cell[r+y][c+x] = 1
-					if (type == 5) if ((c+x > tw-2 && n==1) || (c+x<1 && n==-1)) fnd = 1
+					if (type == 5) if ((c+x > TW-2 && n==1) || (c+x<1 && n==-1)) fnd = 1
 				}
 				if (type == 4) out += csh[r + (3-c) * 4]
 			}
@@ -66,7 +68,7 @@
 		if (!fnd) x += n
 	}
 
-	function game() {
+	const game = () => {
 		tm++
 		if (tm>20 || dwn) {
 			y++
@@ -77,23 +79,22 @@
 		chk(1)
 	}
 
-	setInterval(game,33)
-
-	function trigger(evt) {
+	const trigger = (evt) => {
 		if (evt.keyCode==37) chk(5,-1)
 		if (evt.keyCode==38) chk(4)
 		if (evt.keyCode==39) chk(5,1)
 		if (evt.keyCode==40) dwn = 1
 	}
 	
+	setInterval(game,33)
 	document.addEventListener('keydown',trigger)
 
 </script>
 
 <svg width=300 height=600>
-	{#each range(th) as r}
-		{#each range(tw) as c}
-			<rect x={cc*c} y={cc*r} width=29 height=29 style='fill:{matrix[r][c]==0? 'gray' : 'black'}' />
+	{#each range(TH) as r}
+		{#each range(TW) as c}
+			<rect x={CC*c} y={CC*r} width=29 height=29 style='fill:{matrix[r][c]==0? 'gray' : 'black'}' />
 		{/each}
 	{/each}
 </svg>
