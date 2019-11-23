17ae85a7d761b123c05eac9bb79c7bab812cbfc0
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-11-23 19:46:49 +0100

    x

--- src/App.svelte
+++ src/App.svelte
@@ -1,66 +1,46 @@
 <script>
+	import range from 'lodash.range'
+	import shuffle from 'lodash.shuffle'
 
-	const bag = {coin:20}
-	const price = {}
-	price.coin = -1
-	price.key = 10
-	price.torch = 10
-	price.knife = 6
-	const mud = {}
+	const cards = shuffle(range(12).map( (i) => ({state:0, x:100*(i%3), y:100*Math.floor(i/3)})))
+	for (const i in range(12)) cards[i].value = i%6
 
-	const make = (s) => {
-		const result = {}
-		for (const thing of s.split(' ')) {
-			result[thing] = result[thing] ? result[thing] + 1 : 1
-		}
-		return result
+	const visible = []
+	const click = (card) => {
+		if (visible.length == 2) {
+			const value = (visible[0].value == visible[1].value) ? 2 : 0
+			visible.pop().state = value
+			visible.pop().state = value
+			cards = cards
+		} 
+		if (card.state == 2) return
+		card.state = 1
+		visible.push(card)
+		cards = cards
 	}
-
-	mud.village = {places:'river shop farm'.split(' '), things:make('coin'), desc:'There is a well here'}
-	mud.shop = {places:'village'.split(' '), things:make('coin key torch'), desc:'The owner greets you!'}
-	mud.river = {places:'farm'.split(' '), things:make('coin'), desc:'It is a cold day'}
-	mud.farm = {places:'village'.split(' '), things:make('coin coin'), desc:'No carrots here'}
-
-	let location = 'village'
-	$: curr = mud[location]
-	$: things = curr.things
-	$: places = curr.places
-
-	const take = (thing) => {
-		if (things[thing]>0) {
-			bag[thing] = bag[thing] ? bag[thing] + 1 : 1
-			things[thing]--
-			bag = bag
-		}
-	}
-
 </script>
 
 <style>
-	.m {margin:2%}
+	.text {
+ 		fill : white;
+		text-anchor : middle;
+		alignment-baseline : middle;
+		font-size : 80px;
+	}
 </style>
 
-<div class=m>
-
-	<p>You are at the {location}.</p>
-
-	<p>{curr.desc}.</p>
-
-	{#each Object.keys(bag) as name}
-		<div>You have {bag[name]} {name}</div>
-	{/each}
-
-	<hr>
-	<hr>
-
-	{#each Object.keys(things) as name}
-		{#if things[name]>0}	
-			<div>Take <button on:click={()=> take(name)}>{name}</button></div>
+<svg width=300 height=400>
+	{#each cards as card}
+		<rect 
+			x=0 
+			y=0
+			width=99 
+			height=99 
+			style='fill:{['red','black','white'][card.state]}' on:click={()=>click(card)}
+			transform='translate({card.x},{card.y})'
+		/>
+		{#if card.state==1}
+			<text x=0 y=0 class=text transform='translate({50+card.x},{55+card.y})'>{card.value}</text>
 		{/if}
 	{/each}
-
-	{#each places as place}
-		<div>Goto <button on:click={()=>location=place}>{place}</button></div>
-	{/each}
-
-</div>
+</svg>
